03e8893c5a3a383582d5a734239266ad9e311baa
acheng <2228741983@qq.com>
2020-08-25 22:40:37 +0800

    add FindNumberOfLIS_673

--- src/com/sgc/leetcode/array/FindNumberOfLIS_673.java
+++ src/com/sgc/leetcode/array/FindNumberOfLIS_673.java
@@ -13,6 +13,8 @@ public class FindNumberOfLIS_673 {
 
 
     public int findNumberOfLIS(int[] nums) {
+
+        // 加点东西
         int N = nums.length;
         if (N <= 1) return N;
         int[] lengths = new int[N]; //lengths[i] = length of longest ending in nums[i]
